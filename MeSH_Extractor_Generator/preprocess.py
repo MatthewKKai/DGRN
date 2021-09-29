763489f6740defe4c3fea53bf8481f9405aabbd2
Loafer <loaferzk@autoreg-3317623.dyn.wpi.edu>
2021-09-29 11:09:47 -0400

    .

--- MeSH_Extractor_Generator/preprocess.py
+++ MeSH_Extractor_Generator/preprocess.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import numpy as np
-import tensorflow as tf
+import json
 
 # Load data and preprocess
+
+path_json = r"/Users/loaferzk/LoaferZK/Research/PhD/MeSH_based/data/cancer_corpus.json"
+with open(path_json, "r") as f:
+    file = json.load(f)
