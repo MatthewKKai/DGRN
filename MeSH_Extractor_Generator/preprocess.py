a0591a5ba134af27fc925fc2e3b7eb398324545b
Kai Zhang <matthewzk@outlook.com>
2021-11-11 01:29:30 +0800

    Update preprocess.py

--- MeSH_Extractor_Generator/preprocess.py
+++ MeSH_Extractor_Generator/preprocess.py
@@ -4,6 +4,6 @@ import json
 
 # Load data and preprocess
 
-path_json = r"/Users/loaferzk/LoaferZK/Research/PhD/MeSH_based/data/cancer_corpus.json"
+path_json = r".cancer_corpus.json"
 with open(path_json, "r") as f:
     file = json.load(f)
