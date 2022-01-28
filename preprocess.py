f5fbf168edd8b0463db9c80ff20a1ef4c5ec97aa
Loafer <loaferzk@autoreg-3317623.dyn.wpi.edu>
2022-01-28 17:07:24 -0500

    update data processing

--- preprocess.py
+++ preprocess.py
@@ -4,34 +4,54 @@ import numpy as np
 import re
 import csv
 import nltk
+import pubmed_parser as pp
 
 corpus_path = r"../data/cancer_corpus.json"
 triple_path = r"../data/sldb_complete_triple.csv"
 
 def get_corpus(corpus_path):
-    abstract_pair = {}
-    with open(corpus_path, encoding = "utf-8") as f:
-        data = json.load(f)
-        i = 0
-        for key0 in data.keys():
-            temp = data[key0]
-            for tmp_data in temp:
-                try:
-                    citing_abstract = re.search("p>.*</p", tmp_data["citing_abstract"]).group().strip("p>").strip("</p")
-                    cited_abstract = re.search("p>.*</p", tmp_data["cited_abstract"]).group().strip("p>").strip("</p")
-                    citing_abstract_words = []
-                    cited_abstract_words = []
-                    for word in nltk.sent_tokenize(citing_abstract):
-                        citing_abstract_words.append(nltk.word_tokenize(word))
-                    for word in nltk.sent_tokenize(cited_abstract):
-                        cited_abstract_words.append(nltk.word_tokenize(word))
-                    tmp_dict = {"citing_abstract":citing_abstract_words, "citied_abstract":cited_abstract_words}
-                    abstract_pair[i] = tmp_dict
-                    i += 1
-                    # print("Citing_Abstract:{0}\n----\nCited_Abstract:{1}\n****\n".format(citing_abstract,cited_abstract))
-                except Exception as e:
-                    pass
+    # abstract_pair = {}
+    # with open(corpus_path, encoding = "utf-8") as f:
+    #     data = json.load(f)
+    #     i = 0
+    #     for key0 in data.keys():
+    #         temp = data[key0]
+    #         for tmp_data in temp:
+    #             try:
+    #                 citing_abstract = re.search("p>.*</p", tmp_data["citing_abstract"]).group().strip("p>").strip("</p")
+    #                 cited_abstract = re.search("p>.*</p", tmp_data["cited_abstract"]).group().strip("p>").strip("</p")
+    #                 citing_abstract_words = []
+    #                 cited_abstract_words = []
+    #                 for word in nltk.sent_tokenize(citing_abstract):
+    #                     citing_abstract_words.append(nltk.word_tokenize(word))
+    #                 for word in nltk.sent_tokenize(cited_abstract):
+    #                     cited_abstract_words.append(nltk.word_tokenize(word))
+    #                 tmp_dict = {"citing_abstract":citing_abstract_words, "citied_abstract":cited_abstract_words}
+    #                 abstract_pair[i] = tmp_dict
+    #                 i += 1
+    #                 # print("Citing_Abstract:{0}\n----\nCited_Abstract:{1}\n****\n".format(citing_abstract,cited_abstract))
+    #             except Exception as e:
+    #                 pass
+    abstract = get_abstract(corpus_path)
+    intro = get_intro(corpus_path)
+    citances = get_citances(corpus_path)
+
     return abstract_pair
+
+
+def get_intro(corpus_path):
+    intro = []
+    for item in pp.pubmed_oa_parser(corpus_path):
+        if item["section"] == "Introduction":
+            intro.append(item["text"])
+    return intro    #list type
+
+
+def get_abstract(corpus_path):
+    return pp.parse_pubmed_xml(corpus_path)["abstract"]    # str type
+
+def get_citances(corpus_path):
+    pass
 
 def triple_annotator(triple_path, corpus):
     triple_data = pd.read_csv(triple_path, encoding="utf-8")
@@ -60,5 +80,5 @@ with open("../data/annotated_data.csv", "w") as f:
     for key, value in annotated_data.items():
         writer.writerow([key, value])
 
-print(len(abstract_pair))
-print(len(annotated_data))
+# print(len(abstract_pair))
+# print(len(annotated_data))
