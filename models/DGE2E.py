8addadaeb8b7100837e21222a3f647f7c335c435
Loafer <matthewzk@outlook.com>
2022-03-03 15:28:02 -0500

    update DGE2E framework

--- models/DGE2E.py
+++ models/DGE2E.py
@@ -3,3 +3,19 @@ from torch import nn
 import transformers
 from transformers import BertModel
 import dgl
+
+
+class DGE2E_Encoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def encde(self):
+        pass
+
+
+class DGE2E_Decoder(nn.Moudle):
+    def __int__(self):
+        pass
+
+    def decoe(self):
+        pass
