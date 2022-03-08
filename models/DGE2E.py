0dbef6b82bb2447d9d34ff0353e78813b493566b
Loafer <matthewzk@outlook.com>
2022-03-08 14:02:22 -0500

    update DGE2E framework

--- models/DGE2E.py
+++ models/DGE2E.py
@@ -3,10 +3,13 @@ from torch import nn
 import transformers
 from transformers import BertModel
 import dgl
+import Config
 
 
 class DGE2E_Encoder(nn.Module):
     def __init__(self):
+        self.config = Config
+        self.bert = BertModel.from_pretrained(self.config.bert_path)
         pass
 
     def encde(self):
