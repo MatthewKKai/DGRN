a17a99653e24b9306a4bfecd6a5c582f2d736c54
Loafer <matthewzk@outlook.com>
2022-03-11 11:21:05 -0500

    update DGE2E settings

--- models/DGE2E.py
+++ models/DGE2E.py
@@ -9,7 +9,7 @@ import Config
 class DGE2E_Encoder(nn.Module):
     def __init__(self):
         self.config = Config
-        self.bert = BertModel.from_pretrained(self.config.bert_path)
+        self.bio_bert = BertModel.from_pretrained(self.config.bio_bert_path)
         pass
 
     def encde(self):
