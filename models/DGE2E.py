79161a921696aed1c5e7fba70e4ad3eb5e67eb30
Loafer <matthewzk@outlook.com>
2022-03-17 14:55:56 -0400

    update DGE2E

--- models/DGE2E.py
+++ models/DGE2E.py
@@ -1,3 +1,7 @@
+import numpy as np
+import pandas as pd
+import scispacy
+import spacy
 import torch
 from torch import nn
 import transformers
@@ -10,7 +14,7 @@ class DGE2E_Encoder(nn.Module):
     def __init__(self):
         self.config = Config
         self.bio_bert = BertModel.from_pretrained(self.config.bio_bert_path)
-        pass
+
 
     def encde(self):
         pass
@@ -21,4 +25,14 @@ class DGE2E_Decoder(nn.Moudle):
         pass
 
     def decoe(self):
+        pass
+
+class graph():
+    def __int__(self):
+        pass
+
+    def create_dynamic_graph(self):
+        pass
+
+    def create_statistic_graph(self):
         pass
