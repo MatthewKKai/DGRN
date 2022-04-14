8ef6bb292cf72e4f20e2c21a383890f940e5d3ba
Loafer <matthewzk@outlook.com>
2022-04-14 16:14:43 -0400

    update structure

--- models/DGE2E.py
+++ models/DGE2E.py
@@ -7,6 +7,7 @@ from torch import nn
 from transformers import BertModel
 import dgl
 import Config
+from .utils import RelGraphConvLayer, RelEdgeLayer, Attention
 
 
 class DGE2E_Encoder(nn.Module):
@@ -20,6 +21,10 @@ class DGE2E_Encoder(nn.Module):
         pass
 
 
+    def forward(self):
+        pass
+
+
 class DGE2E_Decoder(nn.Moudle):
     def __int__(self):
         self.cell = nn.RNNCell()
@@ -28,4 +33,6 @@ class DGE2E_Decoder(nn.Moudle):
     def decoe(self):
         pass
 
+    def forward(self):
+        pass
 
