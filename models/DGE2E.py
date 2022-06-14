c84d6a89577e77eddcddf78f901785b62a7fc0e8
Loafer <matthewzk@outlook.com>
2022-06-14 14:53:59 -0400

    update dgg

--- models/DGE2E.py
+++ models/DGE2E.py
@@ -11,10 +11,11 @@ from .utils import RelGraphConvLayer, RelEdgeLayer, Attention
 
 
 class DGE2E_Encoder(nn.Module):
-    def __init__(self,Config):
-        self.config = Config
+    def __init__(self,config):
+        self.config = config
         self.bio_bert = BertModel.from_pretrained(self.config.bio_bert_path)
         self.dropout = nn.Dropout(self.config.dropout)
+
 
 
     def encde(self):
@@ -26,9 +27,10 @@ class DGE2E_Encoder(nn.Module):
 
 
 class DGE2E_Decoder(nn.Moudle):
-    def __int__(self):
+    def __int__(self, encoder, config):
         self.cell = nn.RNNCell()
-
+        self.encoder = encoder
+        self.config = config
 
 
     def decoe(self):
@@ -40,8 +42,16 @@ class DGE2E_Decoder(nn.Moudle):
 
 
 class Dynamic_Graph_Generator(nn.Module):
-    def __init__(self, entity_ls, G_s):
-        pass
+    def __init__(self, entity_ls, G_s, config):
+        self.config = config
+        self.bio_bert = BertModel.from_pretrained(self.config.bio_bert_path)
+        # randomly mask entity and generate labels
+        self.labels = np.zeros(len(entity_ls))
+        self.mask = np.random.randint(low=0, high=len(entity_ls), size=(0.2*len(entity_ls),))
+        for i in range(len(entity_ls)):
+            if i in self.mask:
+                self.labels[i] = 1
+
 
     def forward(self):
         pass
@@ -49,8 +59,13 @@ class Dynamic_Graph_Generator(nn.Module):
 
 
 class Attetntion(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, src_size, trg_size):
+        self.W = nn.Bilinear
+        self.softmax = nn.Softmax(dim=-1)
 
-    def forward(self):
-        pass
+    def forward(self, src, trg):
+        score = self.W(src.unsqueeze(0).expand(trg.size(0), -1), trg)
+        score = self.softmax(score)
+        value = torch.mm(score.permute(1, 0), trg)
+
+        return score.squeeze(0), value.squeeze(0)
